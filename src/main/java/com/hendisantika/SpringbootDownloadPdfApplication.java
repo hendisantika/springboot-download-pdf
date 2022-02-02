03828d1921f4b5ae949c2f081d002f5e1e643199
Hendi Santika <hendisantika@gmail.com>
2022-02-03 06:15:11 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/SpringbootDownloadPdfApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDownloadPdfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDownloadPdfApplication.class, args);
+    }
+
+}
