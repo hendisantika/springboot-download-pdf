7374d9add680be52b38ad60f50f86f353dd8159d
Hendi Santika <hendisantika@gmail.com>
2022-02-03 06:20:43 +0700

    #8 - Add exportReceiptPdf in ExportPdfService

--- src/main/java/com/hendisantika/service/ExportPdfService.java
+++ src/main/java/com/hendisantika/service/ExportPdfService.java
@@ -1,10 +1,17 @@
 package com.hendisantika.service;
 
+import com.lowagie.text.DocumentException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +28,25 @@ public class ExportPdfService {
 
     @Autowired
     private TemplateEngine templateEngine;
+
+    public ByteArrayInputStream exportReceiptPdf(String templateName, Map<String, Object> data) {
+        Context context = new Context();
+        context.setVariables(data);
+        String htmlContent = templateEngine.process(templateName, context);
+
+        ByteArrayInputStream byteArrayInputStream = null;
+        try {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            ITextRenderer renderer = new ITextRenderer();
+            renderer.setDocumentFromString(htmlContent);
+            renderer.layout();
+            renderer.createPDF(byteArrayOutputStream, false);
+            renderer.finishPDF();
+            byteArrayInputStream = new ByteArrayInputStream(byteArrayOutputStream.toByteArray());
+        } catch (DocumentException e) {
+            logger.error(e.getMessage(), e);
+        }
+
+        return byteArrayInputStream;
+    }
 }
