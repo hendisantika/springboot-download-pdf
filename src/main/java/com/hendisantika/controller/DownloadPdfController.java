04eb8910d2c4c2f890a795986b410206dae9bce9
Hendi Santika <hendisantika@gmail.com>
2022-02-03 06:21:59 +0700

    #11 - Add downloadReceipt in DownloadPdfController

--- src/main/java/com/hendisantika/controller/DownloadPdfController.java
+++ src/main/java/com/hendisantika/controller/DownloadPdfController.java
@@ -1,9 +1,16 @@
 package com.hendisantika.controller;
 
 import com.hendisantika.service.ExportPdfService;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,5 +30,14 @@ public class DownloadPdfController {
     @RequestMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/downloadReceipt")
+    public void downloadReceipt(HttpServletResponse response) throws IOException {
+        Map<String, Object> data = createTestData();
+        ByteArrayInputStream exportedData = exportPdfService.exportReceiptPdf("receipt", data);
+        response.setContentType("application/octet-stream");
+        response.setHeader("Content-Disposition", "attachment; filename=receipt.pdf");
+        IOUtils.copy(exportedData, response.getOutputStream());
     }
 }
