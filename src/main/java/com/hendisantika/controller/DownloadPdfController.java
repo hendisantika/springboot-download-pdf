dd9669125b81a2316532c6e05ada76b791325844
hendisantika <hendisantika@yahoo.co.id>
2024-04-15 07:47:09 +0700

    #75 - Upgraded into Spring Boot 3.2.4 version ✏️📗 :octocat:⬆

--- src/main/java/com/hendisantika/controller/DownloadPdfController.java
+++ src/main/java/com/hendisantika/controller/DownloadPdfController.java
@@ -3,13 +3,13 @@ package com.hendisantika.controller;
 import com.hendisantika.entity.Customer;
 import com.hendisantika.entity.ReceiptItem;
 import com.hendisantika.service.ExportPdfService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletResponse;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
